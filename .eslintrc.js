7d7ea79efb424fcf854d34882e88ca6b2caddf7f
Samuelux979 <holaguenas1@gmail.com>
2025-05-17 11:20:54 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: 'eslint:recommended',
   rules: {
     'no-cond-assign': ['error', 'always'],
-    indent: ['error', 2],
+    'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
+    'quotes': ['error', 'single'],
     'no-unused-vars': 'off',
-    semi: ['error', 'always'],
-  },
+    'semi': ['error', 'always']
+  }
 };
